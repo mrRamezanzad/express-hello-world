1337382b75e0561c51ee5d362f690d46c9980899
mrRamezanzad <8212163+mrRamezanzad@users.noreply.github.com>
2024-10-25 14:08:54 +0330

    change

--- app/controllers/hello_world_controller.js
+++ app/controllers/hello_world_controller.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).json({name: "hello"})
+    res.status(202).send()
+})
+
+app.get('/users', (req, res) => {
+    res.status(201).send('users')
 })
 
 module.exports = app
