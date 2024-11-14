3a9ead41e00acf092c2973ac1208d2a82f8cdf75
mrRamezanzad <8212163+mrRamezanzad@users.noreply.github.com>
2024-11-14 22:38:27 +0330

    test: 4

--- app/controllers/hello_world_controller.js
+++ app/controllers/hello_world_controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('hello 3')
+    res.status(200).send('hello 4')
 })
 
 app.get('/users', (req, res) => {
