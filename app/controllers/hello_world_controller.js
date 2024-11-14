e7e8238b5e38abc2c0910e321aa5e96b92a2c0db
mrRamezanzad <8212163+mrRamezanzad@users.noreply.github.com>
2024-11-14 09:40:13 +0330

    feat: send message in index default api

--- app/controllers/hello_world_controller.js
+++ app/controllers/hello_world_controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send()
+    res.status(200).send('hello there')
 })
 
 app.get('/users', (req, res) => {
