6043b97ba8613ea66ad91482c8cdaf665c5128d6
mrRamezanzad <8212163+mrRamezanzad@users.noreply.github.com>
2024-12-12 08:55:38 +0330

    fix: log response statusCode to see what is the problem

--- app/controllers/hello_world_controller.js
+++ app/controllers/hello_world_controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('hello 3')
+    res.status(200).send('hello')
 })
 
 app.get('/users', (req, res) => {
--- test/helloworld.spec.js
+++ test/helloworld.spec.js
@@ -5,6 +5,7 @@ const { app } = require('../app/controllers/hello_world_controller');
 describe('hello world', () => {
     it('responds with 200', () => {
         return request(app).get('/').then(response => {
+            console.log('response.statusCode', response.statusCode)
             expect(response.statusCode).toBe(200)
         });
     });
